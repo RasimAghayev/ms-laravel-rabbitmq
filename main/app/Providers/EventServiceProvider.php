fc5d45c2e099c74eafde2c15f45e539874647a84
Rasim Aghayev <rasimaqayev@gmail.com>
2022-07-11 03:02:55 +0400

    Data Consistency Between Microservices

--- main/app/Providers/EventServiceProvider.php
+++ main/app/Providers/EventServiceProvider.php
@@ -2,17 +2,18 @@
 
 namespace App\Providers;
 
-use App\Jobs\TestJob;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Jobs\ProductCreated;
+use App\Jobs\ProductUpdated;
+use App\Jobs\ProductDeleted;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
 
     public function boot()
     {
-        \App::bindMethod(TestJob::class.'@handle',fn($job)=>$job->handle());
+        \App::bindMethod(ProductCreated::class.'@handle',fn($job)=>$job->handle());
+        \App::bindMethod(ProductUpdated::class.'@handle',fn($job)=>$job->handle());
+        \App::bindMethod(ProductDeleted::class.'@handle',fn($job)=>$job->handle());
     }
 }
